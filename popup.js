b6fb5002007a81e2854da443a6032e05fc69fb9c
Bryan Chen <bchen21@mit.edu>
2019-09-14 23:06:30 -0400

    active indicator

--- popup.js
+++ popup.js
@@ -17,17 +17,30 @@ $('#submitter').on("click", function() {
 
 updateList = () => {
     chrome.storage.sync.get('urlBlacklist', function(result) {
-        let list = result.urlBlacklist || [];
-        $('#urlBlacklist').empty();
-        list.forEach(element => {
-            let x = document.createElement("BUTTON");
-            x.classList.add("delete");
-            x.innerHTML = "&times;";
-            x.onclick = removeItem(element);
-            let node = document.createElement("li");
-            node.appendChild(document.createTextNode(element));
-            node.appendChild(x);
-            $('#urlBlacklist').append(node);
+        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+            let url = tabs[0].url;
+            let isActive = false;
+            let list = result.urlBlacklist || [];
+            $('#urlBlacklist').empty();
+            list.forEach(element => {
+                let x = document.createElement("BUTTON");
+                x.classList.add("delete");
+                x.innerHTML = "&times;";
+                x.onclick = removeItem(element);
+                let node = document.createElement("li");
+                node.appendChild(document.createTextNode(element));
+                node.appendChild(x);
+                $('#urlBlacklist').append(node);
+                // check for activity
+                if (url.match(httpsIfy(element)) || url.match(httpIfy(element))) {
+                    isActive = true;
+                }
+            });
+            if (isActive) {
+                $('#status-circle')[0].classList.add("active-circle");
+            } else {
+                $('#status-circle')[0].classList.remove("active-circle");
+            }
         });
     });
 }
@@ -42,3 +55,7 @@ removeItem = (url) => () => {
         });
     });
 }
+
+const httpsIfy = (url) => "https://*." + url + "/*";
+
+const httpIfy = (url) => "http://*." + url + "/*";
